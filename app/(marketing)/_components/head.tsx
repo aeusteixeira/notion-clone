10f6302efe97e17cdb4465130db9dcad2271919f
Matheus Teixeira <40412362+aeusteixeira@users.noreply.github.com>
2023-10-16 18:32:27 +0000

    Finalizado chamada para ação

---
+++ app/(marketing)/_components/head.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
+const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:6xl">
+        Suas ideias. Documentos & projetos. Unificados. Bem vindo ao{" "}
+        <strong>Jotion</strong>.
+      </h1>
+      <h2 className="text-base sm:text-sm md:text-2xl font-medium">
+        O Jotion é um sistema de gerenciamento de projetos e documentos que
+        permite que você e sua equipe trabalhem juntos de forma mais eficiente.
+      </h2>
+      <Button>
+        Começar
+        <ArrowRight className="h4 w-4 ml-2" />
+      </Button>
+    </div>
+  );
+};
+
+export default Heading;
